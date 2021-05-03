dbce36988bbc41b8826b2a5f26efa55d422d2dfa
Adam Fitzgibbon <fitzgamer@hotmail.com>
2021-05-03 00:17:25 -0500

    added a bunch of new config stuff

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    entities: ["test"],
+    dbName: 'test',
+    type: 'mongo',
+    debug: !__prod__
+    // user credentials?
+  });
+}
+
+main(); 
+
 console.log("Hello World!");
